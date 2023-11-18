02a9a2676ec8bfbdf8419944a75d2ce7a30202fc
Geonu Kim <kgo000926@gmail.com>
2023-11-18 13:49:51 +0900

    init project

---
+++ src/main/java/dt/team7/jbnuselibrary/JbnuSeLibraryApplication.java
@@ -0,0 +1,13 @@
+package dt.team7.jbnuselibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbnuSeLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbnuSeLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dt/team7/jbnuselibrary/JbnuSeLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dt.team7.jbnuselibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbnuSeLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
