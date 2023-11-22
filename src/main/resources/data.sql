69952d52ba0a5f172f5d1f53f0214e425bb65b4b
Geonu Kim <kgo000926@gmail.com>
2023-11-22 22:37:44 +0900

    feat: insert test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,40 @@
+INSERT INTO book (title, link, image, author, publisher, pub_date, isbn, description, count, borrowed_count, lecture)
+VALUES ('네트워크 개론 (4차 산업혁명과 함께하는 네트워크, 3판)',
+        'https://search.shopping.naver.com/book/catalog/32445390378',
+        'https://shopping-phinf.pstatic.net/main_3244539/32445390378.20221227210259.jpg',
+        '진혜진',
+        '한빛아카데미',
+        '20190705',
+        '9791156644521',
+        '기본 이론부터 패킷 분석까지 쉽게 배우는 네트워크',
+        3, 0, '데이터통신');
+
+INSERT INTO book (title, link, image, author, publisher, pub_date, isbn, description, count, borrowed_count, lecture)
+VALUES ('데이터베이스 개론 (제3판)',
+        'https://search.shopping.naver.com/book/catalog/32485531816',
+        'https://shopping-phinf.pstatic.net/main_3248553/32485531816.20230905101442.jpg',
+        '김연희',
+        '한빛아카데미',
+        '20220115',
+        '9791156645771',
+        '기초 개념부터 빅데이터까지 큰 흐름이 보이는 데이터베이스 교과서',
+        5,
+        0,
+        '데이터베이스');
+
+INSERT INTO book (title, link, image, author, publisher, pub_date, isbn, description, count, borrowed_count, lecture)
+VALUES (
+           '쉽게 배우는 알고리즘 (관계 중심의 사고법)',
+           'https://search.shopping.naver.com/book/catalog/32476025996',
+           'https://shopping-phinf.pstatic.net/main_3247602/32476025996.20230919121807.jpg',
+           '문병로',
+           '한빛아카데미',
+           '20180120',
+           '9791156643753',
+           '귀납적 사고를 통한 문제 해결 기법 훈련. 알고리즘에 대한 지식을 기반으로 제대로 프로그래밍을 하는 이들뿐만 아니라, 알고리즘 속에 깃들어 있는 여러 가지 생각하는 방법, 자료구조, 테크닉을 통해 체계적으로 생각하는 훈련을 하고자 하는 모든 이들을 대상으로 합니다. 알고리즘의 설계와 분석을 활용하여 체계적으로 사고할 수 있는 빌딩 블록을 구축하여 컴퓨터 또는 관련 분야의 연구자 또는 개발자로서 갖춰야 할 지적 기반을 쌓을 수 있습니다.',
+           5,
+           0,
+           '컴퓨팅문제와알고리즘'
+       );
+
+
